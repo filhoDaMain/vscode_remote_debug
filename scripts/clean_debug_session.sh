629dee99df080ad654b0d8ad61149a591d7fa348
filhoDaMain <temprilho93@gmail.com>
2026-07-15 00:29:05 +0100

    Make upload and clean more robust
    + Creates upload paths in remote target if they do not exist;
    + Removes uploaded files by name.

--- scripts/clean_debug_session.sh
+++ scripts/clean_debug_session.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+source include/files.inc
 source ../ENV
 
-DEBUGGEE_APPLICATION="$(basename "${ENV_DEBUGGEE_APPLICATION}")"
-
+# Stop gdb server in remote target
 STOP_GDBSERVER_CMD="killall gdbserver > /dev/null 2>&1 &"
-REMOVE_DEBUGGEE_CMD="rm -f \
-    ${ENV_APPLICATION_UPLOAD_PATH}${DEBUGGEE_APPLICATION} \
-    ${ENV_LIBS_UPLOAD_PATH}/* \
-"
-
 ssh ${ENV_TARGET_SSH_HOST} -f ${STOP_GDBSERVER_CMD}
+
+# Remove companion libs in remote target
+DEBUGGEE_LIBS_ABSPATH=
+DEBUGGEE_LIBS=
+if [ -n "${ENV_DEBUGGEE_LIBS_PATH}" ]; then
+    # Get array of uploaded libraries
+    DEBUGGEE_LIBS_ABSPATH=$(get_file_abs_path ${ENV_DEBUGGEE_LIBS_PATH})
+    mapfile -d '' -t DEBUGGEE_LIBS < <(get_files_in_directory "${DEBUGGEE_LIBS_ABSPATH}")
+
+    # Remove one by one
+    for library in "${DEBUGGEE_LIBS[@]}"; do
+        library_basename="$(basename "${library}")"
+        REMOVE_LIBRARY_CMD="rm ${ENV_LIBS_UPLOAD_PATH}/${library_basename}"
+        ssh ${ENV_TARGET_SSH_HOST} -f ${REMOVE_LIBRARY_CMD}
+    done
+fi
+
+# Remove application
+DEBUGGEE_APPLICATION="$(basename "${ENV_DEBUGGEE_APPLICATION}")"
+REMOVE_DEBUGGEE_CMD="rm ${ENV_APPLICATION_UPLOAD_PATH}${DEBUGGEE_APPLICATION}"
 ssh ${ENV_TARGET_SSH_HOST} -f ${REMOVE_DEBUGGEE_CMD}
--- scripts/upload_debuggee.sh
+++ scripts/upload_debuggee.sh
@@ -3,12 +3,29 @@
 source include/files.inc
 source ../ENV
 
+CREATE_APPLICATION_UPLOAD_PATH_CMD="mkdir -p ${ENV_APPLICATION_UPLOAD_PATH}"
 DEBUGGEE_APPLICATION=$(get_file_abs_path ${ENV_DEBUGGEE_APPLICATION})
 
+CREATE_LIBS_UPLOAD_PATH_CMD="mkdir -p ${ENV_LIBS_UPLOAD_PATH}"
+DEBUGGEE_LIBS_ABSPATH=
 DEBUGGEE_LIBS=
+
 if [ -n "${ENV_DEBUGGEE_LIBS_PATH}" ]; then
-    DEBUGGEE_LIBS=$(get_file_abs_path ${ENV_DEBUGGEE_LIBS_PATH})
-    scp ${DEBUGGEE_LIBS}/* ${ENV_TARGET_SSH_HOST}:${ENV_LIBS_UPLOAD_PATH}
+    # Create directory to upload libs, if it does not exist yet in remote target
+    ssh ${ENV_TARGET_SSH_HOST} -f ${CREATE_LIBS_UPLOAD_PATH_CMD}
+
+    # Get array of libary files to upload
+    DEBUGGEE_LIBS_ABSPATH=$(get_file_abs_path ${ENV_DEBUGGEE_LIBS_PATH})
+    mapfile -d '' -t DEBUGGEE_LIBS < <(get_files_in_directory "${DEBUGGEE_LIBS_ABSPATH}")
+
+    # Upload one by one
+    for library in "${DEBUGGEE_LIBS[@]}"; do
+        scp $library ${ENV_TARGET_SSH_HOST}:${ENV_LIBS_UPLOAD_PATH}
+    done
 fi
 
+# Create directory to upload application, if it does not exist yet in remote target
+ssh ${ENV_TARGET_SSH_HOST} -f ${CREATE_APPLICATION_UPLOAD_PATH_CMD}
+
+# Upload application to debug
 scp ${DEBUGGEE_APPLICATION} ${ENV_TARGET_SSH_HOST}:${ENV_APPLICATION_UPLOAD_PATH}
